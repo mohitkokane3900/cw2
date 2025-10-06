de4a205d37e6b0a7e41aded3ef93d067a1e0b2ca
Mohit Kokane <mohitkokane12@gmail.com>
2025-10-06 15:20:39 -0400

    Project base and home screen list

---
+++ lib/homescreen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Map<String, dynamic>> recipes = [
+    {
+      'title': 'Lemon Pasta',
+      'ingredients': [
+        'Spaghetti',
+        'Lemon',
+        'Butter',
+        'Parmesan',
+        'Salt',
+        'Pepper',
+      ],
+      'steps': [
+        'Boil pasta',
+        'Melt butter',
+        'Add lemon zest',
+        'Toss with pasta',
+        'Season and serve',
+      ],
+    },
+    {
+      'title': 'Avocado Toast',
+      'ingredients': ['Bread', 'Avocado', 'Lime', 'Chili Flakes', 'Salt'],
+      'steps': [
+        'Toast bread',
+        'Mash avocado',
+        'Add lime and salt',
+        'Spread and top with chili flakes',
+      ],
+    },
+    {
+      'title': 'Berry Parfait',
+      'ingredients': ['Yogurt', 'Berries', 'Granola', 'Honey'],
+      'steps': [
+        'Layer yogurt',
+        'Add berries',
+        'Sprinkle granola',
+        'Drizzle honey',
+      ],
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Recipes')),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: recipes.length,
+          itemBuilder: (context, i) {
+            final r = recipes[i];
+            return ListTile(
+              title: Text(r['title']),
+              subtitle: Text(
+                '${(r['ingredients'] as List).length} ingredients',
+              ),
+              trailing: const Icon(Icons.chevron_right),
+              onTap: () {},
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
